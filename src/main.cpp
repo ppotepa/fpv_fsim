144f93b19e1b564ef8c41d98c6fe990316025935
Paweł Potępa <potepapawel@gmail.com>
2025-09-15 04:03:43 +0200

    Implement default assets with rotating Earth sphere rendering

--- src/main.cpp
+++ src/main.cpp
@@ -12,9 +12,27 @@
 #include "vehicles/DroneBuilder.h"
 #include <iostream>
 #include <chrono>
+#include <SDL2/SDL.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
 
 int main()
 {
+    // SDL Init
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+    SDL_Window* window = SDL_CreateWindow("Drone Sim", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_OPENGL);
+    SDL_GLContext context = SDL_GL_CreateContext(window);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(45.0, 800.0/600.0, 0.1, 100.0);
+    glMatrixMode(GL_MODELVIEW);
+    glEnable(GL_DEPTH_TEST);
+    GLUquadric* quad = gluNewQuadric();
+    float angle = 0.0f;
+
+    // Simulation init
     EventBus eventBus;
     SimClock simClock(1.0f / 60.0f); // 60Hz physics
     World world(eventBus);
@@ -36,27 +54,43 @@ int main()
     world.addSystem(std::make_unique<VehicleControlSystem>(eventBus));
 
     // Use builder to create entity
-    DroneBuilder droneBuilder(*xmlParser);
-    std::unique_ptr<Entity> drone = droneBuilder.build("configs/drone_default.xml", eventBus);
-    world.addEntity(std::move(drone));
+    // DroneBuilder droneBuilder(*xmlParser);
+    // std::unique_ptr<Entity> drone = droneBuilder.build("configs/drone_default.xml", eventBus);
+    // world.addEntity(std::move(drone));
 
     // Main simulation loop
     auto lastFrameTime = std::chrono::high_resolution_clock::now();
-    while (true)
-    { // exit condition
+    while (true) {
+        SDL_Event e;
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT) goto end;
+        }
+
         auto currentFrameTime = std::chrono::high_resolution_clock::now();
         std::chrono::duration<float> frameDeltaTime = currentFrameTime - lastFrameTime;
         lastFrameTime = currentFrameTime;
 
         simClock.tick(frameDeltaTime.count());
-        while (simClock.shouldStepPhysics())
-        {
+        while (simClock.shouldStepPhysics()) {
             world.update(simClock.getFixedTimestep());
         }
 
+        // Rendering
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glLoadIdentity();
+        glTranslatef(0.0f, 0.0f, -5.0f);
+        glRotatef(angle, 0.0f, 1.0f, 0.0f);
+        glColor3f(0.0f, 0.0f, 1.0f);
+        gluSphere(quad, 1.0f, 20, 20);
+        SDL_GL_SwapWindow(window);
+
+        angle += 1.0f;
         std::cout << "Simulation step" << std::endl;
-        // For demo, break after one step
-        break;
     }
+end:
+    gluDeleteQuadric(quad);
+    SDL_GL_DeleteContext(context);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
