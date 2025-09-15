aa3aa5b202293599b2f1ebd894f35a5dc04dce01
Paweł Potępa <potepapawel@gmail.com>
2025-09-15 03:50:16 +0200

    Implement 2.1-assets-logic structure with asset packages, build tools, and runtime directories

---
+++ assets/build_tools/asset_compiler/main.cpp
@@ -0,0 +1,7 @@
+#include &lt;iostream&gt;
+
+int main(int argc, char* argv[]) {
+    std::cout &lt;&lt; "Asset Compiler CLI Tool" &lt;&lt; std::endl;
+    // TODO: Implement XML -&gt; IR -&gt; cooked binaries
+    return 0;
+}
---
+++ assets/build_tools/codegen/main.cpp
@@ -0,0 +1,7 @@
+#include &lt;iostream&gt;
+
+int main(int argc, char* argv[]) {
+    std::cout &lt;&lt; "Codegen Tool for Asset IDs" &lt;&lt; std::endl;
+    // TODO: Generate headers/tables for AssetId mappings
+    return 0;
+}
