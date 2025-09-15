16ccbec3b1f830e94b54f3a0d114fdd6fabba9f6
Paweł Potępa <potepapawel@gmail.com>
2025-09-15 04:01:43 +0200

    Manual edits to build tools and updated documentation files

--- assets/build_tools/asset_compiler/main.cpp
+++ assets/build_tools/asset_compiler/main.cpp
@@ -1,7 +1,12 @@
-#include &lt;iostream&gt;
+#include &lt; iostream & gt;
 
-int main(int argc, char* argv[]) {
-    std::cout &lt;&lt; "Asset Compiler CLI Tool" &lt;&lt; std::endl;
+int main(int argc, char *argv[])
+{
+    std::cout &lt;
+    &lt;
+    "Asset Compiler CLI Tool" & lt;
+    &lt;
+    std::endl;
     // TODO: Implement XML -&gt; IR -&gt; cooked binaries
     return 0;
 }
--- assets/build_tools/codegen/main.cpp
+++ assets/build_tools/codegen/main.cpp
@@ -1,7 +1,12 @@
-#include &lt;iostream&gt;
+#include &lt; iostream & gt;
 
-int main(int argc, char* argv[]) {
-    std::cout &lt;&lt; "Codegen Tool for Asset IDs" &lt;&lt; std::endl;
+int main(int argc, char *argv[])
+{
+    std::cout &lt;
+    &lt;
+    "Codegen Tool for Asset IDs" & lt;
+    &lt;
+    std::endl;
     // TODO: Generate headers/tables for AssetId mappings
     return 0;
 }
